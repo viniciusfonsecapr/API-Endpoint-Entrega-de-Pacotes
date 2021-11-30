8f835b49e15aa26ca05072ce10aab663ed45e6e1
Vinicius Gabriel Fonseca <87347314+viniciusfonsecapr@users.noreply.github.com>
2021-11-29 23:11:20 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+
+const express = require('express')
+const uuid = require('uuid')
+const port = 3000
+
+const app = express()
+
+app.use(express.json())
+
+
+
+
+
+const users = []
+
+const checkuserId = (request, response, next) => {
+    const { id } = request.params
+
+    const index = users.findIndex(user => user.id === id)
+
+    if (index < 0) {
+        return response.status(404).json({ message: "User not found" })
+    }
+
+    request.userIndex = index
+    request.userId = id 
+    next()
+}
+
+app.get('/users', (request, response) => {
+
+    return response.json({ users })
+})
+
+app.post('/users', (request, response) => {
+    const { name, age } = request.body
+
+    const user = { id: uuid.v4(), name, age, }
+
+    users.push(user)
+
+    return response.status(201).json({ user })
+})
+
+app.put('/users/:id', checkuserId, (request, response) => {
+
+    const { name, age } = request.body
+
+    const index = request.userIndex
+    const id =  request.userId 
+
+    const updateUser = { id, name, age }
+
+
+    users[index] = updateUser
+
+    return response.json({ updateUser })
+})
+
+
+app.delete('/users/:id', checkuserId, (request, response) => {
+
+
+    const index = request.userIndex
+
+
+    users.splice(index, 1)
+
+    return response.status(204).json()
+})
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`🚀 Server started on port ${port}`)
+})
